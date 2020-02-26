eff93debdf2617211668beae8ae037f5a9b62ada
David Cantoran <davidcantoran36@gmail.com>
2020-02-26 00:27:14 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -34,7 +34,7 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/add/)
       product = req.params["item"]
-      if !@@items.include?(product)
+      if @@items.include?(product)
         @@cart << product
         resp.write "added #{product}"
       else 
