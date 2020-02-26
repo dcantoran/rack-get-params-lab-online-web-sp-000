7f500709e0edeb08301e6c4494e7cc8be3e795c3
David Cantoran <davidcantoran36@gmail.com>
2020-02-26 00:29:13 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,15 +15,15 @@ class Application
       end 
     end 
     
-    # if req.path.match(/add/)
-    #   product = req.params["item"]
-    #   if !@@items.include?(product)
-    #     @@cart << product
-    #     resp.write "added #{product}"
-    #   else 
-    #     resp.write "We don't have that item"
-    #   end 
-    # end 
+    if req.path.match(/add/)
+      product = req.params["item"]
+      if !@@items.include?(product)
+        @@cart << product
+        resp.write "added #{product}"
+      else 
+        resp.write "We don't have that item"
+      end 
+    end 
 
     if req.path.match(/items/)
       @@items.each do |item|
@@ -32,14 +32,14 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-    elsif req.path.match(/add/)
-      added_item = req.params["item"]
-      if @@items.include?(added_item)
-        @@cart << product
-        resp.write "added #{added_item}"
-      else 
-        resp.write "We don't have that item"
-      end
+    # elsif req.path.match(/add/)
+    #   added_item = req.params["item"]
+    #   if @@items.include?(added_item)
+    #     @@cart << product
+    #     resp.write "added #{added_item}"
+    #   else 
+    #     resp.write "We don't have that item"
+    #   end
     else
       resp.write "Path Not Found"
     end
